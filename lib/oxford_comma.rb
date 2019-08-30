d42177d66ceb8c24cf1f38ba13ab9e5f36938bcd
wei chen <weichen9083@gmail.com>
2019-08-30 19:29:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+ x = array.size
 
+   if x == 1
+     "#{array[0]}"
+   elsif x == 2
+     array.join(" and ")
+   else 
+     array[-1] = "and #{array[-1]}"
+     array.join(", ")
+   end 
+     
+ 
 end
+
